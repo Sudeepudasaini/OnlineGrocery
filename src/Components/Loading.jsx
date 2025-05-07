4bc4b6c4490728aa6e4c3cdc72ef23eeedd1837a
Sudeeppudasaini <heroclan82@gmail.com>
2025-05-07 21:52:59 +0545

    added

--- src/Components/Loading.jsx
+++ src/Components/Loading.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Loading = () => {
   return (
-    <div>
-      
+    <div className='flex justify-center items-center h-screen'>
+      <div className='animate-spin rounded-full h-24 w-24 border-4 border-gray-300 border-t-[var(--color-primary)]'></div>
     </div>
   )
 }
